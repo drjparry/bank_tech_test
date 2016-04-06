051aa9997a656931a087a5dcf0c23e4e5893208b
David Parry <drjparry@gmail.com>
2016-04-06 15:36:26 +0200

    initial commit

---
+++ spec/bank_spec.rb
@@ -0,0 +1,12 @@
+require 'Bank'
+
+describe "Bank" do
+  it "has a balance fo zero by default" do
+    bank = Bank.new
+    expect(bank.balance).to eq 0
+  end
+
+  it "allows a user to deposit mon" do
+
+  end
+end
